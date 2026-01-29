858d1db71df1a896c4dc5667bf97cbebc07de49a
Matt <matthewklinko@gmail.com>
2026-01-30 02:14:27 +1100

    chore: remove unused validateTraits function
    The validateTraits function was exported but never called anywhere.
    The transformTrait function remains as it's actively used by Tokens.ts.
    
    Also removes the unused Ajv import.

--- src/utils/traits.utils.ts
+++ src/utils/traits.utils.ts
@@ -1,37 +1,3 @@
-import Ajv from "ajv"
-
-export function validateTraits(jsonData: Trait | Trait[]) {
-  const ajv = new Ajv();
-  const dataType = Array.isArray(jsonData) ? "array" : "object"
-  const schema = {
-    type: dataType,
-    items: {
-      type: "object",
-      required: ["traitType", "value"],
-      properties: {
-        traitType: { type: "string" },
-        value: { type: ["string", "number"] }
-      }
-    }
-  };
-  const validate = ajv.compile(schema);
-  const isValid = validate(jsonData);
-
-  if (!isValid) {
-    console.log('--------------------------------------------------------------------------------');
-
-    console.log("INVALID TRAIT FORMAT");
-    console.log('--------------------------------------------------------------------------------'); console.log(validate.errors);
-  } else {
-    console.log('--------------------------------------------------------------------------------');
-
-    console.log("VALID TRAIT FORMAT");
-    console.log('--------------------------------------------------------------------------------');
-
-  }
-  return isValid
-}
-
 export function transformTrait(jsonArray: Trait[]) {
   const groupedObjects: any = {};
 
